2346869bc14a2b7db09ded9fb7041e171bf8149c
Tero Hongisto <tehongis@gmail.com>
2022-11-27 21:14:20 +0200

    fixed rendering.. added rotation.. broke lightlogic.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 let camera, scene, renderer;
 let cameraOrtho, sceneOrtho;
-let model;
 let controls;
-let angle;
-let group;
+var model = new THREE.Group();
 
-function drawship() {
+var angle;
+var oldtime;
+
+function drawShip() {
+    angle = Math.random() * 360;
+
     init();
     animate();
 }
 
 function init() {        
-
-        //var angle = Math.random() * 360;
-        document.getElementById("angle").innerHTML =  angle;
 
         // Sprites
         const lightSprite = new THREE.TextureLoader().load( 'sprite.png' );
@@ -22,38 +22,6 @@ function init() {
         const materialGreenSprite = new THREE.SpriteMaterial( { map: lightSprite, color: 0x00ff00 } );
         const materialYellowSprite = new THREE.SpriteMaterial( { map: lightSprite, color: 0xffff00 } );
 
-        const materialBoat = new THREE.MeshPhongMaterial({ 
-            color: 0xf0f0ff,
-            specular: 100, 
-            shininess: 10,
-            reflectivity: 0.2 });
-
-        // Boat model
-        const loader = new THREE.STLLoader()
-        loader.load("sailboat.stl", function (geometry) {            
-            var middle = new THREE.Vector3();
-            geometry.computeBoundingBox();
-            geometry.boundingBox.getCenter(middle);   
-            model = new THREE.Mesh(geometry, materialBoat)
-            model.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation( -middle.x, -middle.y, -middle.z ) );
-            model.castShadow = true
-            model.receiveShadow = true  
-            geometry.center()
-        })
-
-
-        // Plane
-        const planeGeometry = new THREE.PlaneGeometry(20000, 20000);
-        const materialPlane = new THREE.MeshPhongMaterial({ 
-                                color: 0x2f2f3f,
-                                specular: 20, 
-                                shininess: 90,
-                                reflectivity: 0.9 }); 
-
-        const plane = new THREE.Mesh( planeGeometry, materialPlane );
-        plane.rotateX(-Math.PI / 2);
-        plane.receiveShadow = true;
-        plane.position.set(0, -2.3, 0);
 
         var elem = document.getElementById("view");
 
@@ -66,11 +34,44 @@ function init() {
         scene = new THREE.Scene();
         scene.add(new THREE.HemisphereLight(0x4f4f4f, 0.5));
         //scene.add(new THREE.AmbientLight(0x1a1a3a, 0.15));
-        scene.fog = new THREE.Fog( 0x000000, 1, 50 );
+        scene.fog = new THREE.Fog( 0x000000, 1, 80 );
+      
+        const materialBoat = new THREE.MeshPhongMaterial({ 
+            color: 0xf0f0ff,
+            specular: 100, 
+            shininess: 10,
+            reflectivity: 0.2 });
 
+
+
+        // Boat model
+        const loader = new THREE.STLLoader()
+        loader.load("sailboat.stl", function (geometry) {
+            mesh = new THREE.Mesh(geometry, materialBoat)
+            var middle = new THREE.Vector3();
+            geometry.computeBoundingBox();
+            geometry.boundingBox.getCenter(middle);   
+            mesh.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation( -middle.x, -middle.y, -middle.z ) );
+            mesh.castShadow = true
+            mesh.receiveShadow = true  
+            geometry.center()
+            model.add(mesh);
+        })
+        model.rotateY(THREE.MathUtils.degToRad(angle));
+//        document.getElementById("angle").innerHTML =  angle;
+        
+        // Plane
+        const planeGeometry = new THREE.PlaneGeometry(2000, 2000);
+        const materialPlane = new THREE.MeshPhongMaterial({ 
+                                color: 0x2f2f3f,
+                                specular: 20, 
+                                shininess: 90,
+                                reflectivity: 0.9 }); 
+        const plane = new THREE.Mesh( planeGeometry, materialPlane );
+        plane.rotateX(-Math.PI / 2);
+        plane.receiveShadow = true;
+        plane.position.set(0, -2.3, 0);
         scene.add(plane);
-        scene.add(model);
-//        model.rotateY(THREE.MathUtils.degToRad(angle));
 
         // renderer
         renderer = new THREE.WebGLRenderer({ antialias: false, alpha: false });
@@ -80,15 +81,15 @@ function init() {
         //document.body.appendChild( renderer.domElement );
 
         
-        /*
-        controls = new THREE.OrbitControls(camera, renderer.domElement);
+/*        
+        controls = new THREE.OrbitControls(model, renderer.domElement);
         controls.enableDamping = true;
         controls.rotateSpeed = 0.05;
         controls.dampingFactor = 0.1;
         controls.enableZoom = true;
         controls.autoRotate = true;
         controls.autoRotateSpeed = .75;
-        */
+*/        
 
         //const spriteMastRed = new THREE.Sprite( materialRedSprite );
         //spriteMastRed.position.set(0, 2.7, 0);
@@ -130,6 +131,8 @@ function init() {
             model.add( spriteStarboard );
         }
 
+        scene.add(model);
+
         // compass
         const geometryCircle = new THREE.CircleGeometry( 80, 32 );
         const materialCircle = new THREE.MeshBasicMaterial( { color: 0x3f3f3f } );
@@ -137,14 +140,17 @@ function init() {
         circle.position.set(-220, 150, 0);
         sceneOrtho.add( circle );
 
-        const materialLine = new THREE.LineBasicMaterial( { color: 0xffffff } );     
+        const materialLine = new THREE.LineBasicMaterial( { color: 0xffffff } );
         const points = [];
         points.push( new THREE.Vector3( 0, 0, 0 ) );
+        points.push( new THREE.Vector3( 0, 75, 0 ) );
+        points.push( new THREE.Vector3( 5, 20, 0 ) );
+        points.push( new THREE.Vector3( -5, 20, 0 ) );
         points.push( new THREE.Vector3( 0, 75, 0 ) );
         const geometryLine = new THREE.BufferGeometry().setFromPoints( points );
         const line = new THREE.Line( geometryLine, materialLine );
         line.rotateZ(THREE.MathUtils.degToRad(angle));
-        line.position.set(-220, 150, 0);
+        line.position.set( -220, 150, 0);
         sceneOrtho.add( line );
     
 }
@@ -164,14 +170,27 @@ function onWindowResize() {
 
 
 function render() {
-
     const time = Date.now() / 1000;
-    angle = time * 0.5;
-    angle = angle % 360;
-    //controls.update();
+    var timediff = time - oldtime;
+/*    
+    angle = angle + timediff * 0.5;
+    if (angle > 360) {
+        angle = angle - 360;
+    }
+    if (angle < 0) {
+        angle = angle + 360;
+    }
+*/
+//    controls.update();
+    document.getElementById("angle").innerHTML =  timediff.toPrecision(2) + " : " + angle;
+    model.rotateY(THREE.MathUtils.degToRad(0.1));
+
+
+    
+
     renderer.clear();
     renderer.render( scene, camera );
     renderer.clearDepth();
     renderer.render( sceneOrtho, cameraOrtho );
-
+    oldtime=time;
 }
